84d955fd2d7980d2b618abd81f2c0e45a17334db
DemiShinigami <asifurrahman920@gmail.com>
2020-09-12 12:52:10 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://asif:asif@cluster0.gvuee.mongodb.net/<dbname>?retryWrites=true&w=majority',
+{useNewUrlParser:true})
+.then(()=>console.log("DB connected"))
+.catch(err=> console.error(err));
+
+
+app.get('/', (rez,res)=>{
+    res.send('hello world')
+});
+
+
+app.listen(5000);
